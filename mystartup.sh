09bd9fc73f506b502410227e251d616c2fb1f036
johanwiner <johan.winer@hotmail.com>
2018-10-13 17:01:05 +0200

    Add increasing intensity and color shift
    Takes into consideration if the bulb is set to requested
    intensity already. This needs to be done for other requests
    as well.

--- mystartup.sh
+++ mystartup.sh
@@ -21,21 +21,21 @@ W_TIME_END="07:30"
 Z_ALARM_HOUR="00"
 Z_ALARM_MIN="00"
 
-RGB_CURVE_SIZE=18
-RGB_CURVE=(245 24 5\
-	252 38 11 \
-	245 62 9 \
-	242 88 13 \
-	243 124 2\
-	239 141 11)
+RGB_STEPS=8
+# Can take up to 10 steps with these settnngs but light is too white in the end.
+RGB_START=(215 55 5)
+RGB_STEP=(3 13 7)
 
-RGB_STEPS=10
-RGB_START=(18 28 33)
-#RGB_DIFF=(220 202 70)
-RGB_STEP=(22 20 7)
-#RGB_END=(238 230 103)
+# RGB Curve 1
+#RGB_CURVE_SIZE=18
+#RGB_CURVE=(245 24 5\
+#	252 38 11 \
+#	245 62 9 \
+#	242 88 13 \
+#	243 124 2\
+#	239 141 11)
 
-
+# RGB Curve 2
 #RGB_CURVE_SIZE=27
 #RGB_CURVE=(255 187 123\
 #	255 190 127 \
@@ -47,11 +47,15 @@ RGB_STEP=(22 20 7)
 #	255 245 236 \
 #	255 249 249)
 
+STARTING_INTENSITY=1
+INT_INC_TIME=3000
+
 #Ask for input ugin zenity
-Z_ALARM_HOUR=$(zenity --list --radiolist --width=70 --height=500 --text \
+Z_ALARM_HOUR=$(zenity --list --radiolist --width=70 --height=400 --text \
 	"<b>Please</b> select time for alarm (hours):" \
         --hide-header --column "Select" --column "Hour" \
-        FALSE "05" \
+	TRUE "$(date +%H)" \
+	FALSE "05" \
         FALSE "06" \
         FALSE "07" \
         FALSE "08" \
@@ -76,7 +80,7 @@ then
 	Z_ALARM_MIN=$(zenity --list --radiolist --width=70 --height=400 --text \
         "<b>Please</b> select time for alarm (minutes):" \
         --hide-header --column "Select" --column "Minutes" \
-	FALSE "$(date +%M)" \
+	TRUE "$(date +%M)" \
 	FALSE "00" \
 	FALSE "05" \
         FALSE "10" \
@@ -95,7 +99,6 @@ then
 	then
 		zenity --info --text "Alarm set to $Z_ALARM_HOUR:$Z_ALARM_MIN."
 		W_TIME_START_2=$Z_ALARM_HOUR:$Z_ALARM_MIN
-#		echo $W_TIME_START_2
 		echo "Alarm set to: $W_TIME_START_2" >> ~/Desktop/log.txt
 	else 
 		zenity --info --text "Alarm not set. Exiting"
@@ -104,43 +107,60 @@ else
 	zenity --info --text "Alarm not set. Exiting"
 fi
 
-while [[ "$Z_ALARM_HOUR" != ""  && "$Z_ALARM_MIN" != "" ]]; do
-	DATE=$(eval "date +\"%T\"")
 
-	echo "Timestamp: " $DATE >> ~/Desktop/log.txt
+while [[ "$Z_ALARM_HOUR" != ""  && "$Z_ALARM_MIN" != "" ]]; do
+
+	DATE=$(eval "date +\"%T\"")
+	echo "Timestamp: " $DATE
 
 	#Start alarm
-	echo $(date +%H) " " $Z_ALARM_HOUR " " $(date +%M) " " $Z_ALARM_MIN
-        echo $(date +%H) " " $Z_ALARM_HOUR " " $(date +%M) " " $Z_ALARM_MIN >> ~/Desktop/log.txt
-
 	if [ $(date +%H) = $Z_ALARM_HOUR ] && [ $(date +%M) = $Z_ALARM_MIN ]
 	then 
 		echo "Alarm got off at: " $(eval "date +\"%T\"") >> ~/Desktop/log.txt
-                #zenity --timeout 5 --info --text "Alarm is sounding in 5s"
 
 		# Ramping up bulb
 		while true; do
-			#Await the correct return value of OK: '{"method":"props","params":{"power":"on"}}'
+			# Await the correct return value of OK: '{"method":"props","params":{"power":"on"}}'
+                        RET_VAL=$(echo -ne '{"id":1,"method":"set_power","params":["on","smooth",80000]}\r\n' | nc -w1 192.168.1.150 55443)
+
+                        echo "RET_VAL=$RET_VAL" >> ~/Desktop/log.txt
+
 			if ( echo "$RET_VAL" | grep -q "on" )
 			then 
-				echo "yes,  RET_VAL=$RET_VAL"
+				echo "yes, RET_VAL=$RET_VAL"
 				 break
 			else
 				echo "no, RET_VAL=$RET_VAL"
 			fi
-
-			echo "RET_VAL=$RET_VAL" >> ~/Desktop/log.txt
-			echo "RET_VAL=$RET_VAL"
-			RET_VAL=$(echo -ne '{"id":1,"method":"set_power","params":["on","smooth",120000]}\r\n' | nc -w1 192.168.1.150 55443)
 			sleep 3
                 done
 
-		zenity --timeout 5 --info --text "Light starting up at red"
-		#Change color to red over 3 seconds
-                RET_VAL=$(echo -ne '{"id":1,"method":"set_rgb","params":[11737915,"sudden",0]}\r\n' | nc -w1 192.168.1.150 55443)
-                echo "RET_VAL=$RET_VAL"
+		# Set intensity to minimal 1%.
+		while true; do
+			RET_VAL=$(echo -ne '{"id":1,"method":"set_bright","params":['$STARTING_INTENSITY',"sudden",0]}\r\n' | nc -w1 192.168.1.150 55443)
+			if ( echo "$RET_VAL" | grep -q "bright" )
+                        then 
+                                echo "Set intensity: $RET_VAL"
+                                break
+                        else
+                                echo "Failed to set intensity:$RET_VAL"
+				# Maybe because the intensity is the same.
+				RET_VAL=$(echo -ne '{"id":1,"method":"get_prop","params":["bright"]}\r\n' | nc -w1 192.168.1.150 55443)
+				echo "Reading intensity: $RET_VAL"	
 
-		#Loop over RGB_CURVE
+				if ( echo "$RET_VAL" | grep -q '"1"')
+				then
+					echo "Intensity already set to " $STARTING_INTENSITY
+					break
+				fi
+                        fi
+                        sleep 1
+		done
+
+                # Set starting color of bulb.
+                #let COLOR=${RGB_START[0]}*65536+${RGB_START[1]}*256+${RGB_START[2]}
+                #echo -ne '{"id":1,"method":"set_rgb","params":['$COLOR',"sudden",0]}\r\n' | nc -w1 192.168.1.150 55443
+		# Loop over RGB_CURVE
 		#RGB_COUNTER=0
 		#while [ $RGB_COUNTER -lt $RGB_CURVE_SIZE ]; do
 		#	let COLOR=${RGB_CURVE[$RGB_COUNTER]}*65536+${RGB_CURVE[$RGB_COUNTER+1]}*256+${RGB_CURVE[$RGB_COUNTER+2]}
@@ -152,9 +172,10 @@ while [[ "$Z_ALARM_HOUR" != ""  && "$Z_ALARM_MIN" != "" ]]; do
 		#	sleep 3
 		#done
 
-		#RGB_START=(18 28 33)
-		#RGB_STEP=(22 20 7)
 		RGB_COUNTER=0
+		let INT_STEP=80/$RGB_STEPS
+		let INT=$STARTING_INTENSITY+$INT_STEP
+
                 while [ $RGB_COUNTER -lt $RGB_STEPS ]; do
                        	let COLOR_R=${RGB_START[0]}+${RGB_STEP[0]}*RGB_COUNTER
 			let COLOR_G=${RGB_START[1]}+${RGB_STEP[1]}*RGB_COUNTER
@@ -162,14 +183,26 @@ while [[ "$Z_ALARM_HOUR" != ""  && "$Z_ALARM_MIN" != "" ]]; do
 			let COLOR=$COLOR_R*65536+$COLOR_G*256+$COLOR_B
 
                        	RET_VAL=$(echo -ne '{"id":1,"method":"set_rgb","params":['$COLOR',"sudden",0]}\r\n' | nc -w1 192.168.1.150 55443)
-                       	echo "RET_VAL=$RET_VAL"
-                       	echo "RGB=$COLOR"
-                       	echo "Counter=$RGB_COUNTER"
                        	let RGB_COUNTER=RGB_COUNTER+1
+
+			# Increase intensity also, do this stepwise.
+	                while true; do
+	                        RET_VAL=$(echo -ne '{"id":1,"method":"set_bright","params":['$INT',"smooth",'$INT_INC_TIME']}\r\n' | nc -w1 192.168.1.150 55443)
+	                        if ( echo "$RET_VAL" | grep -q "bright" )
+	                        then 
+	                                echo "Set intensity: $RET_VAL"
+					let INT=$INT+$INT_STEP
+	                                break
+	                        else
+	                                echo "Failed to set intensity:$RET_VAL"
+	                        fi
+	                        sleep 1
+	                done
+
                        	sleep 3
                 done
 
-                zenity --timeout 900 --info --text "Alarm sounding for 15 minutes. Press ok to stop."
+                zenity --timeout 900 --info --width 200 --height 200 --text "Alarm sounding for 15 minutes. Press ok to stop."
 
                 #Turn of bulb slowly after at most 15 minutes.
                 echo -ne '{"id":1,"method":"set_power","params":["off","smooth",5000]}\r\n' | nc -w1 192.168.1.150 55443
@@ -178,10 +211,10 @@ while [[ "$Z_ALARM_HOUR" != ""  && "$Z_ALARM_MIN" != "" ]]; do
 		sleep 1
 		exit 0
 	else 
-		echo "no"
+		echo "Alarm not going off yet"
 	fi
 
-	sleep 50
+	sleep 20
 done
 
 echo "Alarm loop failed at: " $(eval "date +\"%T\"") >> ~/Desktop/log.txt
